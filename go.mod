97d0d3ded6d4e11383f0a630e304fe0404be0068
lucaspopp0 <lucaspopp0@gmail.com>
2026-01-06 15:46:51 -0800

    import c and d

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.25.4
 
 require (
 	github.com/lucaspopp0/go-monorepo-test/a v0.0.2
+	github.com/lucaspopp0/go-monorepo-test/a/c v0.0.0
+	github.com/lucaspopp0/go-monorepo-test/a/c/d v0.0.0
 	github.com/lucaspopp0/go-monorepo-test/b v0.0.1
 )
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package gomonorepotestclient
 
 import (
 	"github.com/lucaspopp0/go-monorepo-test/a"
+	"github.com/lucaspopp0/go-monorepo-test/a/c"
+	"github.com/lucaspopp0/go-monorepo-test/a/c/d"
 	"github.com/lucaspopp0/go-monorepo-test/b"
 )
 
 func main() {
 	_ = a.A{}
 	_ = b.B{}
+	_ = c.C{}
+	_ = d.D{}
 }
